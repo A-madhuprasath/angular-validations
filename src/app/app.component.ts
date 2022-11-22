e94e63ff7ef72dbdce2fa7b5d96b78fecb8e1a34
Madhuveeraprasath <madhu@Madhuveeraprasaths-MacBook-Pro.local>
2022-11-22 22:11:15 +0530

    validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  phoneNumber = '';
+  email = '';
+  phoneNumberValid = false;
+
+  constructor() {
+  }
+
   title = 'dashboard-task';
+  isOpenModal = false;
+
+  openModal(): void {
+    this.isOpenModal = true;
+  }
+
+  closeModal(): void {
+    this.isOpenModal = false;
+  }
+
+  phoneNumberValidation(phone: any): void {
+    if (phone.length === 13 && phone.startsWith('+91')) {
+      this.phoneNumberValid = true;
+    } else {
+      this.phoneNumberValid = false;
+    }
+  }
 }
